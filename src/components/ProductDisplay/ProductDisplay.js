78f27fcc995ce668ea5f36afe1b695ccc33ad7ba
shubhamkataria2005 <katariashubham0101@gmail.com>
2025-05-22 16:22:40 +1200

    Better Image Handling,Rating System,Price Formatting,Button Enhancement,Code Structure & Error Handling.

--- src/components/ProductDisplay/ProductDisplay.js
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -1,37 +1,67 @@
-import React from 'react'
+import React from 'react';
 import './ProductDisplay.css';
+import starIcon from '../../assets/star-icon.png'; // Assume you have a star icon
 
-const ProductDisplay = (props) => {
-    const{product}=props;
+const ProductDisplay = ({ product }) => {
+  // Array of images for the gallery (using the same image as placeholder)
+  const productImages = Array(4).fill(product.image); 
+  
   return (
     <div className='productDisplay'>
       <div className='productDisplay-left'>
-        <div className='product-image'>
-            <img src={product.img} alt=''/>
-            <img src={product.img} alt=''/>
-            <img src={product.img} alt=''/>
-            <img src={product.img} alt=''/>
+        {/* Product image gallery */}
+        <div className='product-gallery'>
+          {productImages.map((img, index) => (
+            <img 
+              key={index} 
+              src={img} 
+              alt={`Product view ${index + 1}`}
+              className='thumbnail'
+            />
+          ))}
         </div>
-        <div className='productDisplay-image'>
-            <img className='main-image' src={product.img} alt=''/>
+        {/* Main product image */}
+        <div className='productDisplay-mainImage'>
+          <img 
+            className='main-image' 
+            src={product.image} 
+            alt={product.name} 
+          />
         </div>
       </div>
+      
       <div className='productDisplay-right'>
+        {/* Product details */}
         <h1>{product.name}</h1>
+        
+        {/* Star rating */}
         <div className='star-rating'>
-            <img src='' alt=''/>
-            <img src='' alt=''/>
-            <img src='' alt=''/>
-            <img src='' alt=''/>
-            <img src='' alt=''/>
+          {[...Array(5)].map((_, i) => (
+            <img 
+              key={i} 
+              src={starIcon} 
+              alt='star rating' 
+              className='star-icon'
+            />
+          ))}
+          <span>({product.rating || 'No reviews'})</span>
         </div>
-        <div className='product-price'>${product.price}</div>
+        
+        {/* Price and description */}
+        <div className='product-price'>${product.price.toFixed(2)}</div>
         <div className='product-description'>{product.description}</div>
         
-      <button>ADD TO CART</button>
+        {/* Add to cart button */}
+        <button 
+          className='add-to-cart-btn'
+          onClick={() => console.log('Added to cart:', product.id)}
+          aria-label={`Add ${product.name} to cart`}
+        >
+          ADD TO CART
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
