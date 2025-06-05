cf957ee9a3731ec237adf3eb8b1611cec7b4b3f1
shubhamkataria2005 <katariashubham0101@gmail.com>
2025-06-05 02:37:25 +0000

    Resolved merge conflict in index.js and added ShopContextProvider

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-import { ShopContext } from './components/Context/ShopContext';
 import ShopContextProvider from './components/Context/ShopContext';
-=======
->>>>>>> a517e045b1014febe2067dce28d35ff7f2bbe603
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-<<<<<<< HEAD
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
-=======
+  <ShopContextProvider>
     <App />
->>>>>>> a517e045b1014febe2067dce28d35ff7f2bbe603
-
+  </ShopContextProvider>
 );
-
