b7209a1b39313d876faddadcd8b7ac2ac14c81f0
shubhamkataria2005 <katariashubham0101@gmail.com>
2025-06-05 02:34:20 +0000

    Resolve merge conflicts in App.js, combine routes and imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-<<<<<<< HEAD
-=======
 import Mens from './pages/Mens';
->>>>>>> a517e045b1014febe2067dce28d35ff7f2bbe603
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminDashboard from './pages/AdminDashboard';
@@ -10,21 +7,15 @@ import BuyerDashboard from './pages/BuyerDashboard';
 import SellerDashboard from './pages/SellerDashboard';
 import ShopCategory from './pages/ShopCategory';
 import Navbar from './components/navbar/navbar';
-<<<<<<< HEAD
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+
 import PreLoved_banner from './components/Assets/preloved banner.jpeg';
 import Clothing_banner from './components/Assets/Clothing_Accessories banner.jpeg';
 import Electrnic_Banner from './components/Assets/Electronics_Gadgets.jpg';
 import Home_banner from './components/Assets/Home_Living.jpg';
 import Kids_banner from './components/Assets/Kids_Baby_Items.webp';
-import Vechile_banner from './components/Assets/Vehicles_Automotive.jpeg'
-
-
-function AppWrapper() {
-  return (
-    <>
-=======
+import Vechile_banner from './components/Assets/Vehicles_Automotive.jpeg';
 
 function AppWrapper() {
   const location = useLocation();
@@ -33,30 +24,37 @@ function AppWrapper() {
   return (
     <>
       {!hideNavbar && <Navbar />}
->>>>>>> a517e045b1014febe2067dce28d35ff7f2bbe603
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-        <Route path="/BuyerDashboard/*" element={<BuyerDashboard />} />
-        <Route path="/sellerDashboard" element={<SellerDashboard />} />
-        <Route path="/Clothing_Accessories" element={<ShopCategory banner={Clothing_banner}category="Clothing_Accessories" />} />
-        <Route path="/Electronics_Gadgets" element={<ShopCategory banner={Electrnic_Banner}category="Electronics_Gadgets" />} />
-        <Route path="/Home_Living" element={<ShopCategory banner={Home_banner}category="Home_Living" />} />
-        <Route path="/Kids_Baby_Items" element={<ShopCategory banner={Kids_banner}category="Kids_Baby_Items" />} />
-        <Route path="/Vehicles_Automotive" element={<ShopCategory banner={Vechile_banner}category="Vehicles_Automotive" />} />
-        <Route path="/product/:productID" element={<Product />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-=======
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/buyer/*" element={<BuyerDashboard />} />
         <Route path="/seller" element={<SellerDashboard />} />
         <Route path="/Mens" element={<Mens />} />
->>>>>>> a517e045b1014febe2067dce28d35ff7f2bbe603
+        <Route
+          path="/Clothing_Accessories"
+          element={<ShopCategory banner={Clothing_banner} category="Clothing_Accessories" />}
+        />
+        <Route
+          path="/Electronics_Gadgets"
+          element={<ShopCategory banner={Electrnic_Banner} category="Electronics_Gadgets" />}
+        />
+        <Route
+          path="/Home_Living"
+          element={<ShopCategory banner={Home_banner} category="Home_Living" />}
+        />
+        <Route
+          path="/Kids_Baby_Items"
+          element={<ShopCategory banner={Kids_banner} category="Kids_Baby_Items" />}
+        />
+        <Route
+          path="/Vehicles_Automotive"
+          element={<ShopCategory banner={Vechile_banner} category="Vehicles_Automotive" />}
+        />
+        <Route path="/product/:productID" element={<Product />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </>
   );
