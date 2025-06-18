b230f0e5f436d4fd9e0844b988485b3cbb2165f9
Sandhu0265 <Sehbaj0265@gmail.com>
2025-06-18 15:32:07 +1200

    fix the wrong imported css file

--- src/pages/resetPassword/ResetPassword.js
+++ src/pages/resetPassword/ResetPassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ResetPasswordStyle from './ResetPassword.css';
+import './ResetPassword.css';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from "../../Firebase";
 
